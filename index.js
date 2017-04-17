e0d252a8677408d8670db11fa51476dff538cf68
Anson <s1496k030@gmail.com>
2017-04-17 23:32:31 +0800

    pick 7 numbers instead 6

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ var Combinatorics = require('js-combinatorics');
     winnings.push({m: 3, s: null, bouns: 400});
 
     var allNumbers = new Array(46).fill(1).map(function (v, i) {return v + i;});
-    var allCombinations = Combinatorics.bigCombination(allNumbers, 6);
+    var allCombinations = Combinatorics.bigCombination(allNumbers, 7);
 	var randomIndex = Math.floor(Math.random() * allCombinations.length);
 	var c = null;
     while((c = allCombinations.next()) && (--randomIndex));
-	console.log(c);
+	console.log(c); // Winning numbers.
 }());
